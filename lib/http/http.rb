d3d57b3b9e9fe803d485f01373f10dbb1667dc6f
Zachary Chai <zachary.chai@outlook.com>
2015-07-01 21:11:14 +0100

    add a module to perform http requests

--- lib/http/http.rb
+++ lib/http/http.rb
@@ -1,1 +1,28 @@
+require 'httparty'
 
+module HTTP
+
+	class Basement
+		include HTTParty
+	end
+
+	class Client
+		class << self
+			
+			def host= newHost
+				Basement.base_uri newHost
+			end
+
+			def get path, options = {}
+				Basement.get(path, options);
+			end
+
+			def post path, options = {}
+				Basement.post(path, options);
+			end
+		end
+	end
+end
+
+# HTTP::Client.host = 'google.ca'
+# puts HTTP::Client.get '/'
