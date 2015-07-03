f7f3b3eead89315b80373030b46a4cf01aa0bfa9
Zachary Chai <zachary.chai@outlook.com>
2015-07-03 20:37:41 +0100

    add http test

--- test/http/http_test.rb
+++ test/http/http_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 describe HTTP do
-  it "must get a 200 response" do
+
+  it "must receive a 200 response" do
     response = HTTP::Client.get 'http://www.google.com'
+    response.code.must_equal 200
+  end
+
+  it "must perform a get using the host" do
+  	HTTP::Client.host = 'google.com'
+  	response = HTTP::Client.get '/'
     response.code.must_equal 200
   end
 end
